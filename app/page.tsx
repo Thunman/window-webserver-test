61c142075273a9ae4f381f82c02cedbab43c082d
Thunman <thunman42@gmail.com>
2024-03-22 22:11:16 +0100

    adding a wargames style frontpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,59 @@
+"use client";
+import { useState, useEffect, useRef } from "react";
+import "../styles/styles.css";
+
 export default function Home() {
-  return <h1>Hello</h1>;
+  const [showPrompt, setShowPrompt] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const text = "Hello, would you like to play a game?";
+  const typingSpeed = 80;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowPrompt(true);
+    }, text.length * typingSpeed);
+
+    return () => clearTimeout(timer);
+  }, [text]);
+
+  useEffect(() => {
+    if (showPrompt && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [showPrompt]);
+
+  return (
+    <div className="main-container">
+      <h3
+        className="typed"
+        style={{
+          animation: `typing ${(text.length * typingSpeed) / 1000}s steps(${
+            text.length
+          }, end) forwards, blinking 1s ${
+            (text.length * typingSpeed) / 1000
+          }s 1 forwards`,
+        }}
+      >
+        {text}
+      </h3>
+      {showPrompt && (
+        <div style={{ display: "flex", color: "green" }}>
+          <h3>#:</h3>
+          <input
+            ref={inputRef}
+            style={{
+              background: "transparent",
+              border: "none",
+              outline: "none",
+              color: "green",
+              fontSize: "1.17em",
+              fontWeight: "bold",
+            }}
+            className="blinking-cursor"
+            spellCheck="false"
+          ></input>
+        </div>
+      )}
+    </div>
+  );
 }
