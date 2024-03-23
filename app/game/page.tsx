8987ac1de7a91381f30fba6047bd310cf7e09e39
Thunman <thunman42@gmail.com>
2024-03-23 22:22:58 +0100

    adding routing

---
+++ app/game/page.tsx
@@ -0,0 +1,8 @@
+const Game = () => {
+  return (
+    <div>
+      <h1 style={{ color: "green" }}>Game</h1>
+    </div>
+  );
+};
+export default Game;
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Wargames from "@/components/wargames";
+import Wargames from "./wargames";
 
 export default function Home() {
   return <Wargames />;
